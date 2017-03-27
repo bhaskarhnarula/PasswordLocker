232867efff44461e299804a178e445a4d7f5717d
Bhaskar <bhaskarhnarula@gmai.com>
2017-03-27 15:23:12 +0530

    - Adding SpringBoot Main Class

---
+++ src/main/java/com/home/locker/passwordlocker/PasswordLockerMainModule.java
@@ -0,0 +1,25 @@
+package com.home.locker.passwordlocker;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"application-context.xml"})
+public class PasswordLockerMainModule extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+
+	public static void main(String[] args) {
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(PasswordLockerMainModule.class).bannerMode(Banner.Mode.OFF);
+	}
+}
